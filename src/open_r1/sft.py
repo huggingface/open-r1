f0307f0a5adce44521fee10b11896757dafeb6af
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2025-01-25 12:38:55 +0000

    Fix slurm

--- src/open_r1/sft.py
+++ src/open_r1/sft.py
@@ -27,13 +27,13 @@ accelerate launch --config_file=configs/zero3.yaml src/open_r1/sft.py \
     --packing \
     --max_seq_length 4096 \
     --per_device_train_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 4 \
     --gradient_checkpointing \
     --bf16 \
     --logging_steps 5 \
     --eval_strategy steps \
     --eval_steps 100 \
-    --output_dir data/Qwen2.5-1.5B-Distill-R1
+    --output_dir data/Qwen2.5-1.5B-Open-R1-Distill
 """
 
 import argparse
