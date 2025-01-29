e3b5e2ea2f3fad0dbdd6afa27cd4e8034510bddd
CohenQU <yuxiao1155@gmail.com>
2025-01-29 20:10:12 +0000

    update bash scripts to integrate vllm

--- bash/r1.sh
+++ bash/r1.sh
@@ -21,14 +21,15 @@ final_reward_weights=(
 
 for alpha in "${alphas[@]}"; do
     for weight in "${final_reward_weights[@]}"; do
+        task=Llama-3.2-3B-Instruct_backtrack_grpo_${alpha}_${weight}
         accelerate launch --config_file grpo-metaMATH/configs/zero3.yaml grpo-metaMATH/src/open_r1/grpo.py \
             --model_name_or_path ${model} \
             --dataset_name ${data} \
             --dataset_start 0 \
             --dataset_end 20000 \
             --reward_funcs "final" "info_gain" \
-            --alpha 0.1 \
-            --final_reward_weight "ttf" \
+            --alpha ${alpha} \
+            --final_reward_weight ${weight} \
             --learning_rate 1.0e-6 \
             --lr_scheduler_type cosine \
             --warmup_ratio 0.1 \
@@ -38,7 +39,10 @@ for alpha in "${alphas[@]}"; do
             --gradient_accumulation_steps 32 \
             --max_prompt_length 1500 \
             --max_completion_length 1024 \
-            --num_generations 4 \
+            --num_generations 8 \
+            --use_vllm True \
+            --vllm_gpu_memory_utilization 0.8 \
+            --temperature 0.9 \
             --logging_steps 1 \
             --eval_strategy no \
             --save_strategy "steps" \
