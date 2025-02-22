58cbde43267685cd6071ca332041b115ebf24b04
Marco Zocca <marco.zocca@unfoldml.com>
2025-02-22 07:56:56 +0100

    construct prompt in the dataset generator

--- src/open_r1/rewards/code/htgen.py
+++ src/open_r1/rewards/code/htgen.py
@@ -5,6 +5,50 @@ from open_r1.rewards.api.code.unfoldml.htgen import gen_triples_33, verify_tripl
 
 # # GRPOTrainer requires 1. a dataset and 2. a verification callback
 
+def mk_dataset_row(o):
+    """
+    Construct the prompt from the raw API data
+    """
+    label = o['label']
+    pre = o['pre']
+    program = o['program'] # list of statements
+    post = o['post']
+
+    program_str = '\n'.join(program)
+
+    label_is_total = label == 'ok_total' 
+
+    prompt_hdr = (
+        f"Below you are given a Python program triple, made of a precondition predicate, "
+        f"a sequence of program statements, and a post-condition predicate."
+        f"The precondition returns True if the variable environment before beginning the "
+        f"program execution satisfies the predicate, and False otherwise. "
+        f"Similarly, the postcondition returns True if the program environment after the last "
+        f"statement satisfies the predicate, and False otherwise. "
+        f"Note that there might be unsatisfiable or contradictory predicates, that make the solution unreachable."
+        f"With this information, you should judge whether the program is 'total', i.e. "
+        f"whether the post-condition evaluates to True for all possible variable assigments "
+        f"that satisfy the precondition."
+    )
+
+    prompt_question = (
+        f"Given a program triple made of program '{program_str}', preconditions '{pre}' and postcondition '{post}', is the postcondition "
+        f"always True at the end of the program ? Please return 'True' or 'False'."
+    )
+
+    # # concatenate header and question into a prompt
+    prompt_problem = f"{prompt_hdr}\n{prompt_question}"
+
+    solution = label_is_total  # boolean
+
+    # # construct a row of the dataset
+    o_out = {
+        "problem": prompt_problem,
+        "solution": label_is_total
+    }
+
+    return o_out
+
 
 def mk_dataset(
     max_ast_depth:int = 3, 
@@ -16,15 +60,18 @@ def mk_dataset(
     """
     construct an interable dataset for GRPOTrainer
     """
-    dataset = IterableDataset.from_generator(
-        gen_triples_33(
+    gen = gen_triples_33(
             max_ast_depth = max_ast_depth, 
             n_stmt = n_stmt,
             n_pre_terms = n_pre_terms, 
             n_post_terms = n_post_terms,
             seed = seed,
             )
-    )
+
+    # produce prompts from the raw API data
+    gen_prompts = (mk_dataset_row(o) for o in gen if o is not None)
+
+    dataset = IterableDataset.from_generator(gen_prompts)
     return dataset
 
 def total_correctness_reward(completions, solution, **kwargs):
