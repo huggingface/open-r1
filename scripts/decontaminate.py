a1495873becbf83c923498d3e8ceeb114438cc3f
plaguss <agustin@argilla.io>
2025-02-24 19:54:23 +0100

    Add license header and attribution to the authors

--- scripts/decontaminate.py
+++ scripts/decontaminate.py
@@ -1,4 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """
+This script is used to decontaminate a dataset by checking for n-gram overlap with other datasets.
+It uses the same approach presented in https://arxiv.org/abs/2501.19393,
+as found in: https://github.com/simplescaling/s1/blob/main/data/decontaminate_util.py
+
 python scripts/decontaminate.py \
     --dataset "open-r1/verifiable-coding-problems-python" \
     --split train \
