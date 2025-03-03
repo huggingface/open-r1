aa97e62cef99e7c318c5de75eb7c295d534f61e2
Marco Z <ocramz@users.noreply.github.com>
2025-03-03 04:08:43 +0000

    add test for verify v2 endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 import unittest
 
-from open_r1.rewards_internal.api.code.unfoldml.htgen import gen_triples, verify_triple
+from open_r1.rewards_internal.api.code.unfoldml.htgen import gen_triples, verify_triple, verify_triple_v2
 
 
 class TestApi(unittest.TestCase):
@@ -24,6 +24,20 @@ class TestApi(unittest.TestCase):
         o = verify_triple(is_total=is_total, preconditions=preconditions, program=program, postconditions=post_not_ok)
         res_not_ok = o["prediction_is_correct"]
         self.assertEqual(res_not_ok, False)
+
+    def test_verify_v2_triple_result(self):
+        preconditions = "True"  # trivial precondition
+        program = "v4 = (0 - v3)\nv3 = v3\nv5 = v4"
+        post_ok = "v5 == (0 - v3)"  # post-condition that verifies
+        post_not_ok = "v5 == (1 - v3)"  # post-condition that does not verify
+        # # should return True
+        o = verify_triple_v2(preconditions=preconditions, program=program, postconditions=post_ok)
+        res_ok = o["result"]
+        self.assertEqual(res_ok, 'proven_total')
+        # # should return False
+        o = verify_triple_v2(preconditions=preconditions, program=program, postconditions=post_not_ok)
+        res_not_ok = o["result"]
+        self.assertEqual(res_not_ok, 'failed')
 
 
 if __name__ == "__main__":
