1d7c743dfe694957fe7eea729fa4191b27ae088d
Aidan Do <aidando73@gmail.com>
2025-03-21 20:19:00 +1100

    .

--- src/open_r1/rewards.py
+++ src/open_r1/rewards.py
@@ -69,7 +69,7 @@ def accuracy_reward(completions, solution, **kwargs):
 
 def format_reward(completions, **kwargs):
     """Reward function that checks if the reasoning process is enclosed within <think> and </think> tags, while the final answer is enclosed within <answer> and </answer> tags."""
-    pattern = r"^<think>\n.*?\n</think>\n<answer>\n.*?\n</answer>$"
+    pattern = r"^<think>.*?</think>.+"
     completion_contents = [completion[0]["content"] for completion in completions]
     matches = [re.match(pattern, content, re.DOTALL | re.MULTILINE) for content in completion_contents]
     return [1.0 if match else 0.0 for match in matches]
