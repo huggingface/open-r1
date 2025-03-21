80795444ba4e01b582f80de082ee91c337d38d68
edbeeching <edbeeching@gmail.com>
2025-03-21 07:39:48 +0000

    finalize slow tests

--- tests/slow/test_code_reward.py
+++ tests/slow/test_code_reward.py
@@ -17,11 +17,12 @@ import unittest
 
 from datasets import load_dataset
 
-from open_r1.rewards import code_reward
+from open_r1.rewards import code_reward, ioi_code_reward
 
 
 class TestCodeRewards(unittest.TestCase):
     def test_python_code_reward(self):
+        # requires E2B, see the README.md file
         code_dataset = load_dataset("open-r1/verifiable-coding-problems-python_decontaminated-tested")
         NUM_SAMPLES = 20
         samples = code_dataset["train"].select(range(NUM_SAMPLES))
@@ -32,7 +33,17 @@ class TestCodeRewards(unittest.TestCase):
         assert rewards == [1.0] * NUM_SAMPLES
 
     def test_ioi_code_reward(self):
-        pass
+        # This slow test case requires spinning up a bunch (I tested with ~64) of piston workers, see docs here
+        # slurm/piston/README.md
+        code_dataset = load_dataset("open-r1/ioi-reward-test-dataset")
+        NUM_SAMPLES = 16
+        samples = code_dataset["train"].select(range(NUM_SAMPLES))
+        test_completions = [[{"content": f"```cpp\n{sample['sample_solution']}```"}] for sample in samples]
+        keys = [key for key in samples[0] if key not in ["prompt", "completion"]]
+        reward_kwargs = {key: [example[key] for example in samples] for key in keys}
+        rewards = ioi_code_reward(test_completions, **reward_kwargs)
+        print(rewards)
+        assert rewards == [1.0] * NUM_SAMPLES
 
 
 if __name__ == "__main__":
