9b420d1f8b1d08661659efae1df10c6929ce9185
edbeeching <edbeeching@gmail.com>
2025-04-03 09:38:43 +0000

    fix import when e2b not present

--- src/open_r1/rewards.py
+++ src/open_r1/rewards.py
@@ -27,12 +27,12 @@ from math_verify import LatexExtractionConfig, parse, verify
 
 from .utils import is_e2b_available
 from .utils.ioi import SubtaskResult, add_includes, get_piston_client_from_env, score_subtask
-from .utils.router_sandbox import BatchedRoutedSandbox
 
 
 if is_e2b_available():
     from dotenv import load_dotenv
     from e2b_code_interpreter import AsyncSandbox
+    from .utils.router_sandbox import BatchedRoutedSandbox
 
     load_dotenv()
 else:
