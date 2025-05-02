e053a8bb46260f6267a6faed8b1dba07cbe56cbd
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2025-05-02 22:09:24 +0000

    Add v0 and v1 revipes

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ _deps = [
     "langdetect",  # Needed for LightEval's extended tasks
     "latex2sympy2_extended>=1.0.6",
     "liger-kernel>=0.5.6",
-    "lighteval @ git+https://github.com/huggingface/lighteval.git@989f5f5586de1ddfeceb0dfa5076bd0740d376fa",
+    "lighteval @ git+https://github.com/huggingface/lighteval.git@tune-pass-at-k",
     "math-verify==0.5.2",  # Used for math verification in grpo
     "packaging>=23.0",
     "parameterized>=0.9.0",
--- src/open_r1/sft.py
+++ src/open_r1/sft.py
@@ -23,7 +23,6 @@ accelerate launch --config_file=recipes/accelerate_configs/zero3.yaml src/open_r
     --dataset_name open-r1/OpenR1-Math-220k \
     --learning_rate 2.0e-5 \
     --num_train_epochs 1 \
-    --packing \
     --max_seq_length 4096 \
     --per_device_train_batch_size 2 \
     --gradient_accumulation_steps 8 \
