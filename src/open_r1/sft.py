25dba465debf4284434fc5946a816aaec84df9c4
Lewis Tunstall <lewis.c.tunstall@gmail.com>
2025-05-27 09:56:38 +0000

    Fix

--- src/open_r1/sft.py
+++ src/open_r1/sft.py
@@ -101,7 +101,7 @@ def main(script_args, training_args, model_args):
     trainer = SFTTrainer(
         model=model,
         args=training_args,
-        train_dataset=dataset[script_args.dataset_train_split].select(range(100)),
+        train_dataset=dataset[script_args.dataset_train_split],
         eval_dataset=(dataset[script_args.dataset_test_split] if training_args.eval_strategy != "no" else None),
         processing_class=tokenizer,
         peft_config=get_peft_config(model_args),
